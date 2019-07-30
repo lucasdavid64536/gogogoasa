c20c7cb0a9877bee36678f31a4551ec28116faa6
lucasdavid64536 <38979101+lucasdavid64536@users.noreply.github.com>
2019-07-30 13:09:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,19 +15,6 @@ from asyncio import sleep
 logging.basicConfig(level='INFO')
 bot = commands.Bot(command_prefix='.')
 bot.remove_command('help')
-OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
-
-def load_opus_lib(opus_libs=OPUS_LIBS):
-    if opus.is_loaded():
-        return True
-    for opus_lib in opus_libs:
-        try:
-            opus.load_opus(opus_lib)
-            return
-        except OSError:
-            pass
-    raise RuntimeError('Could not load an opus lib. Tried %s' % (', '.join(opus_libs)))
-load_opus_lib()
 
 
 @bot.listen()
